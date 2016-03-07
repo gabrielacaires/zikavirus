18b5c2ffe9cabd158aedda8aae7dfc6c2f7d75ef
Gabriela Caires <gabicairesj@gmail.com>
2016-03-07 13:09:00 -0500

    updated nav bar, transmission and added mosquito gif ;)

--- animation.js
+++ animation.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
 
+	/*Slide mosquito*/
+	$(".mosquito").fadeIn(500, function(){
+		$(".mosquito").animate({
+		marginLeft:"+=550px"
+		},1500,function(){
+			$(".mosquito").fadeOut(1000);
+			}
+		);
+	});
+	
+
 	$(".about").on("mouseenter", function(){
 
 		$(".top").css({
@@ -15,20 +26,74 @@ $(document).ready(function(){
 		});
 	});
 
-	$("li").on({
-		mouseenter: function(){
-			$("li").css({
-				borderWidth: "1px",
-				borderStyle: "dashed"	
-			})
-		},
-		mouseleave: function(){
-			$("li").css({
-				borderWidth: "1px",
-				borderStyle: "solid",
-				borderColor: "white"	
-			})
-		}
+	$(".navli").on("mouseenter", function(){
+			$(".navli").css({
+				borderStyle: "dashed",
+				borderColor: "black"
+			});
+		});
+
+	$(".navli").on("mouseleave", function(){
+		$(".navli").css({
+			borderStyle: "solid",
+			borderColor: "white"
+		});
+	});
+
+	$(".navli2").on("mouseenter", function(){
+			$(".navli2").css({
+				borderStyle: "dashed",
+				borderColor: "black"
+			});
+		});
+
+	$(".navli2").on("mouseleave", function(){
+		$(".navli2").css({
+			borderStyle: "solid",
+			borderColor: "white"
+		});
+	});
+
+		$(".navli3").on("mouseenter", function(){
+			$(".navli3").css({
+				borderStyle: "dashed",
+				borderColor: "black"
+			});
+		});
+
+	$(".navli3").on("mouseleave", function(){
+		$(".navli3").css({
+			borderStyle: "solid",
+			borderColor: "white"
+		});
+	});
+
+	$(".navli4").on("mouseenter", function(){
+			$(".navli4").css({
+				borderStyle: "dashed",
+				borderColor: "black"
+			});
+		});
+
+	$(".navli4").on("mouseleave", function(){
+		$(".navli4").css({
+			borderStyle: "solid",
+			borderColor: "white"
+		});
+	});
+
+	$(".navli5").on("mouseenter", function(){
+			$(".navli5").css({
+				borderStyle: "dashed",
+				borderColor: "black"
+			});
+		});
+
+	$(".navli5").on("mouseleave", function(){
+		$(".navli5").css({
+			borderStyle: "solid",
+			borderColor: "white"
+		});
 	});
 
 	$(".about").on("mouseenter", function(){
---
+++ transmission.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+
+	$(".fever").on("mouseenter", function(){
+		$(".feverimg").show(); 
+	});
+
+	$(".fever").on("mouseleave", function(){
+		$(".feverimg").hide();
+	});
+
+	$(".rash").on("mouseenter", function(){
+		$(".rashimg").show(); 
+	});
+
+	$(".rash").on("mouseleave", function(){
+		$(".rashimg").hide();
+	});
+
+	$(".pain").on("mouseenter", function(){
+		$(".painimg").show(); 
+	});
+
+	$(".pain").on("mouseleave", function(){
+		$(".painimg").hide();
+	});
+
+	$(".redeyes").on("mouseenter", function(){
+		$(".redeyesimg").show(); 
+	});
+
+	$(".redeyes").on("mouseleave", function(){
+		$(".redeyesimg").hide();
+	});
+});
